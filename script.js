62365115e99ae5b1efbc365bc97a804d2f0e26ca
perosnal dev <dev.ti@grupoelcerezo.com>
2025-02-11 06:59:11 -0600

    se carga funcionalidad

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var tipoVoto = 0;
+var savevoto = "http://localhost:3001/api/zurich/savevoto";
+const btn_save = document.getElementById("btn_save");
+const btn_cancel = document.getElementById("btn_cancel");
 // Función para alternar el menú en dispositivos móviles
 function toggleMenu() {
   const navbarMenu = document.querySelector('.navbar-menu');
@@ -15,6 +19,7 @@ window.addEventListener('load', adjustHeroHeight);
 
 // Función para abrir el modal de votación
 function openVoteModal(sectionId) {
+
   const modal = document.getElementById('voteModal');
   const image = document.querySelector(`#modalImage img`);
   const title = document.getElementById('modalTitle');
@@ -47,21 +52,100 @@ function closeVoteModal() {
 /// Función para enviar el formulario de votación
 function submitVoteForm(event) {
   event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+btn_cancel.disabled = true;
+btn_save.disabled = true;
+  
 
   // Obtén los datos del formulario
   const sectionTitle = document.getElementById('sectionTitle').value;
   const insurance = document.getElementById('insurance').value;
   const age = document.getElementById('age').value;
   const gender = document.getElementById('gender').value;
+  console.log(sectionTitle, insurance, age, gender)
+  var data = {
+    campania: sectionTitle,
+    seguro: insurance,
+    edad: age,
+    sexo: gender
+}
+
+var options = {
+  method : "POST",
+  body : JSON.stringify(data),
+  headers: {
+      'Content-Type': 'application/json'
+  }
+}
+
+
+
+  fetch(savevoto,options)
+  .then(resp => resp.json())
+  .then(resp => {
+    console.log(resp)
+
+    if (resp.status == "CORRECTO") {
+
+      let timerInterval;
+Swal.fire({
+  title: "CARGANDO SU VOTO, POR FAVOR ESPERE",
+  html: "ENVIANDO VOTACIÓN...",
+  timer: 20000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading();
+    const timer = Swal.getPopup().querySelector("b");
+    timerInterval = setInterval(() => {
+      timer.textContent = `${Swal.getTimerLeft()}`;
+    }, 20000);
+  },
+  willClose: () => {
+    clearInterval(timerInterval);
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    //console.log("I was closed by the timer");
+  }
+});
+      
+  
+      
+     
+
+      setTimeout(() => {
+        Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: "VOTO GENERADO CON ÉXITO, GRACIAS",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      },23000)
+
+      setTimeout(() => {
+        window.location.href = "/gracias.html";
+      },25000)
+    }
+  })
+  .catch(error => {
+    Swal.fire({
+      position: "top-center",
+      icon: "error",
+      title: error.message,
+      showConfirmButton: false,
+      timer: 3000,
+    });
+  })
 
   // Validación: Verificar que todos los campos requeridos estén completos
-  if (insurance && age && gender) {
+ /* if (insurance && age && gender) {
     // Aquí se pueden hacer acciones con los datos, como enviarlos a un servidor
     // Redirigir a otra página después de enviar
     window.location.href = "/gracias.html";
   } else {
     alert("Por favor, complete todos los campos antes de enviar.");
-  }
+  }*/
 }
 
 
